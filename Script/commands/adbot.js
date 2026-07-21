52df51bc2c692b7e96367ed8884e71e4578be949
Tuhin <mhloloddib@gmail.com>
2026-07-21 12:05:42 +0600

    Update admin info in adbot.js

--- Script/commands/adbot.js
+++ Script/commands/adbot.js
@@ -54,7 +54,7 @@ module.exports.run = async ({ api, event, args }) => {
     if (args[0] == "admin") {
         let callback = () => api.sendMessage(
             {
-                body: `———»ADMIN BOT«———\n❯ Name: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔\n❯ Facebook: https://facebook.com/100044713412032\n❯ Thanks for using ${global.config.BOTNAME} bot`,
+                body: `———»ADMIN BOT«———\n❯ Name: 𝐃𝐄𝐕 𝐓𝐔𝐇𝐈𝐍\n❯ Facebook: https://facebook.com/Picci.Tuhin047\n❯ Thanks for using ${global.config.BOTNAME} bot`,
                 attachment: fs.createReadStream(__dirname + "/cache/1.png")
             },
             event.threadID,
